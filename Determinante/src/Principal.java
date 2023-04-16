1fd347baf8c1ee0fd3efab50d429d8cdac265031
Marcos Antônio dos Santos <marcosantonio.dev@gmail.com>
2023-04-16 12:08:29 -0300

    Update Principal.java

--- Determinante/src/Principal.java
+++ Determinante/src/Principal.java
@@ -33,7 +33,44 @@ public class Principal {
 				
 				return diagPrin - diagSecu;
 			}else{
-				return 0;
+				int[][] novaMatriz = new int[matriz.length][5];
+
+				for(int i = 0; i < novaMatriz.length; i++) {
+					for(int j = 0; j < novaMatriz[i].length; j++) {
+						int coluna = j%matriz[i].length;
+
+						novaMatriz[i][j] = matriz[i][coluna];
+					}
+				}
+
+				int[] diagPrinc = {1, 1, 1};
+				int[] diagSec = {1, 1, 1};
+
+
+				for(int i = 0; i < novaMatriz.length; i++) {
+					for(int j = 0; j < novaMatriz[i].length; j++) {
+						if(i == j) {
+							diagPrinc[0] *= novaMatriz[i][j];
+							diagPrinc[1] *= novaMatriz[i][j+1];
+							diagPrinc[2] *= novaMatriz[i][j+2];
+						}
+
+						if(i+j == novaMatriz.length - 1) {
+							diagSec[0] *= novaMatriz[i][j];
+							diagSec[1] *= novaMatriz[i][j+1];
+							diagSec[2] *= novaMatriz[i][j+2];
+						}
+					}
+				}
+
+				int somaPrinc = 0;
+				int somaSec = 0;
+
+				for(int i = 0; i<diagPrinc.length; i++) {
+					somaPrinc += diagPrinc[i];
+					somaSec += diagSec[i];
+				}
+				return somaPrinc - somaSec;
 			}
 		}else {
 			System.out.println("Não eh possivel calcular");
